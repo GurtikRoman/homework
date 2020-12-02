7845f728219bf7a2719314a22e69fd0cf88dd196
GurtikRoman <Gutikrs@gmail.com>
2020-12-02 17:14:16 +0200

    New progejt

--- src/hm1/Main1.java
+++ src/hm1/Main1.java
@@ -1,4 +1,29 @@
 package hm1;
 
+import java.util.Scanner;
+
 public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 'a'");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter 'b'");
+        int b = scanner.nextInt();
+
+        System.out.println("Enter 'c'");
+        int c = scanner.nextInt();
+        //int a = 4; 2
+        //int b = 3; 4
+        //int c = 1; 1
+        int d;
+
+        d = (b * b) - (4 * a * c);
+        if (d > 0) {
+            System.out.println(" x1 = " + (-b + Math.sqrt(d)) / (2 * a));
+            System.out.println(" x2 = " + (-b - Math.sqrt(d)) / (2 * a));
+        } else {
+            System.out.println("D < 0");
+        }
+    }
 }
--- src/hm1/Main2.java
+++ src/hm1/Main2.java
@@ -1,4 +1,40 @@
 package hm1;
 
+import java.util.Scanner;
+
 public class Main2 {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        int numbr= 5;
+        System.out.println("Enter day in week");
+        String day = scaner.nextLine();
+        switch (day){
+            case "Monday":
+                System.out.println("Dancing,");
+                break;
+            case "Tusday":
+                System.out.println("Reading");
+                break;
+            case "Wensday":
+                System.out.println("Footbol");
+                break;
+            case "Thursday":
+                System.out.println("Swiming");
+                break;
+            case  "Friday":
+                System.out.println("Geography");
+                break;
+            case "Saturday":
+                System.out.println("Watch TV");
+                break;
+            case "Sunday":
+                System.out.println("Beach");
+                break;
+
+        }
+
+
+
+    }
+
 }
